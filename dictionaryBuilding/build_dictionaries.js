02d9967a64e0162e9d77188b647ec6aad5f8f506
angus <angussbjohnson@gmail.com>
2023-11-03 10:04:20 +1100

    Try to remove plurals and offensive words

--- dictionaryBuilding/build_dictionaries.js
+++ dictionaryBuilding/build_dictionaries.js
@@ -3,34 +3,36 @@ const tenK = getFiveLetterWordsFrom('tenK.txt')
 const fiveHundredK = getFiveLetterWordsFrom('fiveHundredK.txt')
 const scrabble = getFiveLetterWordsFrom('scrabble.txt')
 const names = getFiveLetterWordsFrom("names.txt")
-// console.log(JSON.stringify(Array.from(names)))
+const commonPlurals = getFiveLetterWordsFrom("commonPlurals.txt")
+const bad = getFiveLetterWordsFrom("bad.txt")
+const manualExclusions = getFiveLetterWordsFrom("manualExclusions.txt")
 
 function getFiveLetterWordsFrom(fileName) {
   return new Set(
     fs.readFileSync(fileName)
       .toString()
-      .split("\n")
+      .split(/\n|\r|\r\n/)
       .map(s => s.trim().replace(",", "").toUpperCase())
       .filter((w) => w.length === 5)
   );
 }
 
 function union(setA, setB) {
-    let _union = new Set(setA)
-    for (let elem of setB) {
-        _union.add(elem)
-    }
-    return _union
+  let _union = new Set(setA)
+  for (let elem of setB) {
+    _union.add(elem)
+  }
+  return _union
 }
 
 function intersection(setA, setB) {
-    let _intersection = new Set()
-    for (let elem of setB) {
-        if (setA.has(elem)) {
-            _intersection.add(elem)
-        }
+  let _intersection = new Set()
+  for (let elem of setB) {
+    if (setA.has(elem)) {
+      _intersection.add(elem)
     }
-    return _intersection
+  }
+  return _intersection
 }
 
 function setMinus(setA, setB) {
@@ -42,7 +44,7 @@ function setMinus(setA, setB) {
 }
 
 
-const small = Array.from(setMinus(intersection(intersection(fiveHundredK, tenK), scrabble), names))
+const small = Array.from(setMinus(setMinus(setMinus(setMinus(intersection(intersection(fiveHundredK, tenK), scrabble), names), commonPlurals), manualExclusions), bad))
 const big = Array.from(union(fiveHundredK, union(tenK, scrabble)))
 
 console.log(`export const big_dictionary = new Set(${JSON.stringify(big)})`)
